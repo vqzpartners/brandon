44e7287b8a87d7b9011e0bb8683fcdd9c3522ab0
u09093023 <189559174+u09093023@users.noreply.github.com>
2025-06-01 15:41:42 -0500

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,8 @@
+(function() {
+  const script = document.createElement('script');
+  script.src = 'https://scripts.simpleanalyticscdn.com/latest.js';
+  script.async = true;
+  script.dataset.collectDnt = 'true';
+
+  document.head.appendChild(script);
+})(); 
