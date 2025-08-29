d414572b64a74f97523ba3e79c787bd4bd1c2d0f
u09093023 <189559174+u09093023@users.noreply.github.com>
2025-08-28 20:31:17 -0500

    Delete analytics.js

--- analytics.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const script = document.createElement('script');
-  script.src = 'https://scripts.simpleanalyticscdn.com/latest.js';
-  script.async = true;
-  script.dataset.collectDnt = 'true';
-
-  document.head.appendChild(script);
-})(); 
